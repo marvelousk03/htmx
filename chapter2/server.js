fa9dec37b6e964f101df0f3ab4ff015e36a7c37c
marvek16 <marvelousk1603f@gmail.com>
2025-03-25 10:33:07 +0200

    commit

---
+++ chapter2/server.js
@@ -0,0 +1,28 @@
+import express from 'express';
+
+const app = express();
+
+// Set static folder
+app.use(express.static('public'));
+
+// Parse URL-encoded bodies (as sent by HTML forms)
+app.use(express.urlencoded({ extended: true }));
+
+// Parse JSON bodies (as sent by API clients)
+app.use(express.json());
+
+app.post('/calculate', (req, res) => {
+    const height = parseFloat(req.body.height);
+    const weight = parseFloat(req.body.weight);
+    const bmi = weight / (height * height);
+
+    res.send(
+        <p>Height of ${height} & Weight of ${weight} gives you BMI of ${bmi.toFixed(2)}</p>
+    );
+})
+
+
+// Start the server
+app.listen(3000, () => {
+    console.log('Server listening on port 3000');
+});
