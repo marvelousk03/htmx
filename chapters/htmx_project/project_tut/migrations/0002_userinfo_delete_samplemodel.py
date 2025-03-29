14795a9d6665a387d0649ea32b6845885c982ad4
marvek16 <marvelousk1603f@gmail.com>
2025-03-29 20:03:49 +0200

    commit

--- chapters/htmx_project/project_tut/migrations/0002_userinfo_delete_samplemodel.py
+++ chapters/htmx_project/project_tut/migrations/0002_userinfo_delete_samplemodel.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('htmx_tutorial', '0001_initial'),
+        ('project_tut', '0001_initial'),
     ]
 
     operations = [
