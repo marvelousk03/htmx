dc0615be5b5d52c912f31b99cc3761140cb4f280
marvek16 <marvelousk1603f@gmail.com>
2025-03-29 20:00:34 +0200

    commit

--- chapters/htmx_project/project_tut/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.example, name='example'),
-    path('sample-post/', views.sample_post, name='sample-post'),
-]
