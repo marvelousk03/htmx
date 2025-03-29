a31ff9be7b3e53654fb083b3faf14c09fe129a31
marvek16 <marvelousk1603f@gmail.com>
2025-03-29 20:08:48 +0200

    commit

--- chapters/project_tut/forms.py
+++
@@ -1,8 +0,0 @@
-# forms.py
-from django import forms
-
-
-class SampleForm(forms.Form):
-    email = forms.CharField()
-    name = forms.CharField(min_length=3)
-    favourite_color = forms.CharField()
